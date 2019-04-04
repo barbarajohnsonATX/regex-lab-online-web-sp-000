dccf7470c090e27168c5b8de100138c4450534bb
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-04 01:15:04 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,11 +1,11 @@
 def starts_with_a_vowel?(word)
   #\A starts with 
   #/i ignore case
-  if word.match(/\A[aeiou]/i) == nil 
-    return true 
+  if word.match(/\A[aeiou]/i) == nil
+    return false
   else
-    return false 
-  end 
+    return true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
