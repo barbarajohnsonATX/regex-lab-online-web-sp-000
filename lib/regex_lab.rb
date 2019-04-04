cbb3b0dfe94d99f836e1de19d09d9b063400ce05
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-04 00:40:15 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/\b[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -8,7 +8,7 @@ end
 
 def words_five_letters_long(text)
 
-end
+end'
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
 
