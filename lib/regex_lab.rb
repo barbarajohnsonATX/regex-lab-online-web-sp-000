1cdae9cba6950102a80d22af39424f85abf0c850
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-04 01:11:52 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -21,7 +21,10 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(/\b\A[[:punct:]]\b/)
+  if text.match(/^[A-Z].*\.$/) == nil 
+    false 
+  else 
+    true 
 end
 
 def valid_phone_number?(phone)
